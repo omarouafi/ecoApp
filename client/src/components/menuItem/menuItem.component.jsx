91501e551fa1140b3febbc12528ba434d9cb1741
omarouafi <omarouafi12@gmail.com>
2023-01-07 20:04:24 +0100

    update

--- client/src/components/menuItem/menuItem.component.jsx
+++ client/src/components/menuItem/menuItem.component.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './menuItem.styles.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./menuItem.styles.scss";
 
-
-const MenuItem = ({imageUrl,title}) => (
-    <div className="menu-item">
-        <div className="background-image" style={{backgroundImage:`url('${imageUrl}')`}} />
-        <div className="content">
-            <span className="title">{title}</span>
-            <span className="subtitle">SHOPNOW</span>
-        </div>
+const MenuItem = ({ imageUrl, title }) => (
+  <Link to={`/shop/${title}`} className="menu-item">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url('${imageUrl}')` }}
+    />
+    <div className="content">
+      <span className="title">{title}</span>
+      <span className="subtitle">SHOPNOW</span>
     </div>
-)
+  </Link>
+);
 
-
-
-
-export default MenuItem
-
+export default MenuItem;
--- client/src/redux/user/user-saga.js
+++ client/src/redux/user/user-saga.js
@@ -1,115 +1,94 @@
-import {takeLatest,all,call,put} from 'redux-saga/effects'
-import { auth, createUserDocument, getCurrentUser, provider } from '../../firebase/firebase.utils'
-import { signinFailure, signinSuccess, signoutSuccess } from './user-actions'
-import { userTypes } from './user-types'
+import { takeLatest, all, call, put } from "redux-saga/effects";
+import {
+  auth,
+  createUserDocument,
+  getCurrentUser,
+  provider,
+} from "../../firebase/firebase.utils";
+import { signinFailure, signinSuccess, signoutSuccess } from "./user-actions";
+import { userTypes } from "./user-types";
 
-
-
-
-
-
-
-function* getUser(user,data) {
-    try {
-        const userRef = yield createUserDocument(user,data)
-        const snapshot = yield userRef.get()
-        yield put(signinSuccess({id:snapshot.id,...snapshot.data()}))
-    } catch (error) {
-        yield put(signinFailure(error.message))
-        
-    }
-
-} 
-
-
-
-function* googleSignIn(){
-
-    try {
-        const {user} = yield auth.signInWithPopup(provider)
-        yield getUser(user)
-    } catch (error) {
-        yield put(signinFailure(error.message))
-    }
+function* getUser(user, data) {
+  try {
+    const userRef = yield createUserDocument(user, data);
+    const snapshot = yield userRef.get();
+    yield put(signinSuccess({ id: snapshot.id, ...snapshot.data() }));
+  } catch (error) {
+    yield put(signinFailure(error.message));
+  }
 }
 
-
-function* signIn({payload:{email,password}}){
-    try {
-        const {user} = yield auth.signInWithEmailAndPassword(email,password)
-        yield getUser(user)
-    } catch (error) {
-        yield put(signinFailure(error.message))
-    }
+function* googleSignIn() {
+  try {
+    const { user } = yield auth.signInWithPopup(provider);
+    yield getUser(user);
+  } catch (error) {
+    yield put(signinFailure(error.message));
+  }
 }
 
-function* signUp({payload:{email,password,displayName}}){
-    try {
-        const {user} = yield auth.createUserWithEmailAndPassword(email,password)
-        yield getUser(user,{displayName})
-    } catch (error) {
-        yield put(signinFailure(error.message))
-    }
+function* signIn({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    yield getUser(user);
+  } catch (error) {
+    yield put(signinFailure(error.message));
+  }
 }
 
-function* signout(){
-    try {
-        yield auth.signOut()
-        yield put(signoutSuccess())
-    } catch (error) {
-        yield put(signinFailure(error.message))
-    }
+function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+    yield getUser(user, { displayName });
+  } catch (error) {
+    yield put(signinFailure(error.message));
+  }
 }
 
-
-
-function* checkUser(){
-    try {
-        const user = yield getCurrentUser()
-        if(!user) return;
-        yield put(signinSuccess(user))   
-    } catch (error) {
-        yield put(signinFailure(error.message))
-    }    
+function* signout() {
+  try {
+    yield auth.signOut();
+    yield put(signoutSuccess());
+  } catch (error) {
+    yield put(signinFailure(error.message));
+  }
 }
 
-
-
-
-
-
-function* signInSaga(){
-    yield takeLatest(userTypes.SIGNIN_START,signIn)
+function* checkUser() {
+  try {
+    const user = yield getCurrentUser();
+    if (!user) return;
+    yield put(signinSuccess(user));
+  } catch (error) {
+    yield put(signinFailure(error.message));
+  }
 }
 
-function* signUpSaga(){
-    yield takeLatest(userTypes.SIGNUP_START,signUp)
+function* signInSaga() {
+  yield takeLatest(userTypes.SIGNIN_START, signIn);
 }
 
-function* googleSignInSaga(){
-    yield takeLatest(userTypes.GOOGLE_SIGNIN_START,googleSignIn)
+function* signUpSaga() {
+  yield takeLatest(userTypes.SIGNUP_START, signUp);
 }
 
-function* signoutSaga(){
-    yield takeLatest(userTypes.SIGNOUT_START,signout)
-}
-function* checkUserSaga(){
-    yield takeLatest(userTypes.CHECK_USER,checkUser)
+function* googleSignInSaga() {
+  yield takeLatest(userTypes.GOOGLE_SIGNIN_START, googleSignIn);
 }
 
-
-
-
-export default function* userRootSaga(){
-    yield all([
-        call(googleSignInSaga),
-        call(signInSaga),
-        call(signUpSaga),
-        call(signoutSaga),
-        call(checkUserSaga),
-    ])
+function* signoutSaga() {
+  yield takeLatest(userTypes.SIGNOUT_START, signout);
+}
+function* checkUserSaga() {
+  yield takeLatest(userTypes.CHECK_USER, checkUser);
 }
 
-
-
-
+export default function* userRootSaga() {
+  yield all([
+    call(googleSignInSaga),
+    call(signInSaga),
+    call(signUpSaga),
+    call(signoutSaga),
+    call(checkUserSaga),
+  ]);
+}
