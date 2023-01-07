acfd7b05170228e9bd72e2ecb65f37e4704606d6
omarouafi <omarouafi12@gmail.com>
2023-01-07 16:02:30 +0100

    update

--- server.js
+++ server.js
@@ -1,45 +1,35 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const path = require('path')
-const stripe = require('stripe')(process.env.STRIPE)
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const stripe = require("stripe")(process.env.STRIPE);
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,'client/build')))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'client/build','index.html'))
-})
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token,
+    amount: req.body.amount,
+    currency: "usd",
+  };
 
+  stripe.charges.create({});
+});
 
-app.post('/payment',(req,res)=>{
-    
-    const body ={
-        source:req.body.token,
-        amount:req.body.amount,
-        currency:'usd'
+const port = process.env.PORT || 3154;
 
-    }
-    
-    stripe.charges.create({
-
-    })
-})
-
-
-
-
-
-const port = 3154 ||  process.env.PORT
-
-app.listen(port,() => {
-    console.log("server started");
-})
+app.listen(port, () => {
+  console.log("server started");
+});
